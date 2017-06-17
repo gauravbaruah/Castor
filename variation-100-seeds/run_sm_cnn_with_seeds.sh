99811ce0ebd26fd943b9cac91d4a754ee534589d
Gaurav Baruah <gbaruah@uwaterloo.ca>
2017-06-16 21:41:55 -0400

    script for running various seeds

---
+++ variation-100-seeds/run_sm_cnn_with_seeds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+if [ "$#" != 3 ]; then
+    echo "usage: this_script.sh 100-random-seeds.txt <start_seed_number> <end_seed_number>"
+    exit
+fi
+
+seed_list=`head -$3 100-random-seeds.txt | tail -$(($3-$2+1))`
+
+pushd ../sm_cnn/
+
+for seed in $seed_list;
+do
+    echo $seed
+    python main.py sm_cnn.seed=$seed.model --paper-ext-feats --random-seed $seed > sm_cnn.seed=$seed.log 2>&1
+done
+
+popd
